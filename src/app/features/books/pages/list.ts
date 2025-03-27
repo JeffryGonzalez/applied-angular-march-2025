c86d4764fc786a476b7e138677b5849214e160ad
Jonathan Morgan <morganprogramming@pm.me>
2025-03-27 20:42:31 +0000

    refactor for drawer

--- src/app/features/books/pages/list.ts
+++ src/app/features/books/pages/list.ts
@@ -16,11 +16,35 @@ import { BookApiEntity } from '../models/BookApiEntity';
 
     <div class="drawer drawer-end">
       <input id="stats-drawer" type="checkbox" class="drawer-toggle" />
+
       <div class="drawer-content">
-        <!-- Page content here -->
         <label for="stats-drawer" class="drawer-button btn btn-primary"
           >Open stats drawer</label
         >
+        <div class="overflow-x-auto">
+          <table class="table table-pin-rows">
+            <thead>
+              <tr>
+                <td>ID</td>
+                <td>Year</td>
+                <td>Title</td>
+                <td>Author</td>
+                <td></td>
+              </tr>
+            </thead>
+            <tbody>
+              @for (book of books(); track $index) {
+                <tr class="hover:bg-base-300">
+                  <td>{{ book.id }}</td>
+                  <td>{{ displayYear(book.year) }}</td>
+                  <td>{{ book.title }}</td>
+                  <td>{{ book.author }}</td>
+                  <td></td>
+                </tr>
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
       <div class="drawer-side">
         <label
@@ -30,31 +54,6 @@ import { BookApiEntity } from '../models/BookApiEntity';
         ></label>
         <app-book-stats [books]="books()!" />
       </div>
-    </div>
-
-    <div class="overflow-x-auto">
-      <table class="table table-pin-rows">
-        <thead>
-          <tr>
-            <td>ID</td>
-            <td>Year</td>
-            <td>Title</td>
-            <td>Author</td>
-            <td></td>
-          </tr>
-        </thead>
-        <tbody>
-          @for (book of books(); track $index) {
-            <tr class="hover:bg-base-300">
-              <td>{{ book.id }}</td>
-              <td>{{ displayYear(book.year) }}</td>
-              <td>{{ book.title }}</td>
-              <td>{{ book.author }}</td>
-              <td></td>
-            </tr>
-          }
-        </tbody>
-      </table>
     </div>
   `,
   styles: ``,
