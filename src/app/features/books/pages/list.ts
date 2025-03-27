ba4e2d7de02a3b744c5ea4b40382408e85c998c6
Nick Tocicki <ntocicki@gmail.com>
2025-03-27 20:44:37 +0000

    Added sorting to book list with clickable columns.

--- src/app/features/books/pages/list.ts
+++ src/app/features/books/pages/list.ts
@@ -20,22 +20,46 @@ export type BookApiResponse = BookApiEntity[];
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [NgIf, NgFor],
   template: `
-    <p>Book List</p>
+    <p>List of Books</p>
     <div
       class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100"
     >
       <table class="table w-full">
         <thead>
           <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Year</th>
+            <th
+              class="clickable-header"
+              (click)="sortColumn = 'id'; sortAscending = !sortAscending"
+            >
+              ID
+            </th>
+            <th
+              class="clickable-header"
+              (click)="sortColumn = 'title'; sortAscending = !sortAscending"
+            >
+              Title
+            </th>
+            <th
+              class="clickable-header"
+              (click)="sortColumn = 'author'; sortAscending = !sortAscending"
+            >
+              Author
+            </th>
+            <th
+              class="clickable-header"
+              (click)="sortColumn = 'year'; sortAscending = !sortAscending"
+            >
+              Year
+            </th>
           </tr>
         </thead>
         <tbody>
           <ng-container *ngIf="books.value() as bookList">
-            <tr *ngFor="let book of sortedBookList(bookList)">
+            <tr
+              *ngFor="
+                let book of sortBooks(bookList, sortColumn, sortAscending)
+              "
+            >
               <td>{{ book.id }}</td>
               <td>{{ book.title }}</td>
               <td>{{ book.author }}</td>
@@ -56,7 +80,52 @@ export class ListComponent {
     loader: () => fetch('/api/books').then((res) => res.json()),
   });
 
-  sortedBookList(bookList: BookApiEntity[]): BookApiEntity[] {
-    return bookList.sort((a, b) => a.title.localeCompare(b.title));
+  sortColumn: keyof BookApiEntity = 'title';
+  sortAscending = true;
+
+  sortBooks(
+    bookList: BookApiEntity[],
+    column: keyof BookApiEntity,
+    ascending: boolean,
+  ): BookApiEntity[] {
+    return [...bookList].sort((first, second) => {
+      const firstComparison = first[column];
+      const secondComparison = second[column];
+
+      // Convert string IDs to numbers for for proper numerical sorting
+      if (column === 'id') {
+        const firstNumericalComparison = parseInt(
+          firstComparison as string,
+          10,
+        );
+        const secondNumericalComparison = parseInt(
+          secondComparison as string,
+          10,
+        );
+        return ascending
+          ? firstNumericalComparison - secondNumericalComparison
+          : secondNumericalComparison - firstNumericalComparison;
+      }
+
+      if (
+        typeof firstComparison === 'string' &&
+        typeof secondComparison === 'string'
+      ) {
+        return ascending
+          ? firstComparison.localeCompare(secondComparison)
+          : secondComparison.localeCompare(firstComparison);
+      }
+
+      if (
+        typeof firstComparison === 'number' &&
+        typeof secondComparison === 'number'
+      ) {
+        return ascending
+          ? firstComparison - secondComparison
+          : secondComparison - firstComparison;
+      }
+
+      return 0;
+    });
   }
 }
