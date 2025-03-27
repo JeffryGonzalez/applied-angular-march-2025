810d26da0f62300644fdbd1552850a73f81c6875
Jonathan Morgan <morganprogramming@pm.me>
2025-03-27 18:36:21 +0000

    Updated books links to remove "hardcoded" in app-section-nav call

--- src/app/features/books/books.ts
+++ src/app/features/books/books.ts
@@ -1,17 +1,16 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
 import { SectionNavComponent } from '../../../shared/components/section-nav/section-nav';
+import { NavBarItem } from '../../components/nav-bar/types';
 
 @Component({
   selector: 'app-books',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [SectionNavComponent],
   template: `
-    <app-section-nav
-      sectionName="Books"
-      [links]="[{ label: 'Book List', href: 'list' }]"
-    >
-    </app-section-nav>
+    <app-section-nav sectionName="Books" [links]="links()"> </app-section-nav>
   `,
   styles: ``,
 })
-export class BooksComponent {}
+export class BooksComponent {
+  links = signal<NavBarItem[]>([{ label: 'Book List', href: 'list' }]);
+}
