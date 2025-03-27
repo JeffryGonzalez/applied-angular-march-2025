132c442d0d1a0d0ec66c0eacffcd629d6867d829
Ed Yu <eddytheflow@gmail.com>
2025-03-27 20:39:29 +0000

    fix the dice display

--- src/app/features/lab-final/components/counter.ts
+++ src/app/features/lab-final/components/counter.ts
@@ -6,53 +6,65 @@ import { DiceStore } from '../services/dice.store';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
   template: `
+    <div class="mt-5 ml-5">
+      <h1>Counter Page</h1>
 
-  <div class="mt-5 ml-5" >
-    <h1>Counter Page</h1>
-
-    <div class="">
-      <div class="flex"><h1>1: </h1>
-      <div *ngFor="let count of store.rollCounts; let i = index">
-
-        <!-- @for (count of store.rollCounts()[0]; track $index) {
-            {{count + 1}}
-          } -->
-      </div>
-        <p>{{this.store.rollCounts()[0]}}</p>
-      </div>
-
-      <div class="flex"><h1>2: </h1>
-        <p>{{this.store.rollCounts()[1]}}</p>
+      <div>
+        @for (roll of store.rollCounts(); track $index) {
+          <div class="flex">
+            <h1>{{ $index + 1 }}:</h1>
+            @for (n of range(roll); track $index) {
+              🎲
+            }
+            <!-- <p>{{ roll }}</p> -->
+          </div>
+        }
       </div>
 
-      <div class="flex"><h1>3: </h1>
-        <p>{{this.store.rollCounts()[2]}}</p>
-      </div>
+      <!-- <div class="">
+        <div class="flex">
+          <h1>1:</h1>
+          <p>{{ this.store.rollCounts()[0] }}</p>
+        </div>
 
-      <div class="flex"><h1>4: </h1>
-        <p> {{this.store.rollCounts()[3]}}</p>
-      </div>
+        <div class="flex">
+          <h1>2:</h1>
+          <p>{{ this.store.rollCounts()[1] }}</p>
+        </div>
 
-      <div class="flex"><h1>5: </h1>
-        <p>{{this.store.rollCounts()[4]}}</p>
-      </div>
+        <div class="flex">
+          <h1>3:</h1>
+          <p>{{ this.store.rollCounts()[2] }}</p>
+        </div>
 
-      <div class="flex"><h1>6: </h1>
-        <p>{{this.store.rollCounts()[5]}}</p>
-      </div>
+        <div class="flex">
+          <h1>4:</h1>
+          <p>{{ this.store.rollCounts()[3] }}</p>
+        </div>
+
+        <div class="flex">
+          <h1>5:</h1>
+          <p>{{ this.store.rollCounts()[4] }}</p>
+        </div>
+
+        <div class="flex">
+          <h1>6:</h1>
+          <p>{{ this.store.rollCounts()[5] }}</p>
+        </div>
+      </div> -->
     </div>
-
-  </div>
-
   `,
-  styles: ``
+  styles: ``,
 })
 export class RollCounterComponent {
-  store = inject(DiceStore)
+  store = inject(DiceStore);
   diceCount: number[] = [0, 0, 0, 0, 0, 0];
 
   increment(roll: number) {
     this.diceCount[roll]++;
   }
 
+  range(n: number): number[] {
+    return Array.from({ length: n }, (_, i) => i);
+  }
 }
